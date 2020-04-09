c28749d70b906fd9a28e6c6a207d15ce5543bff1
p3miki <p3miki@outlook.com>
2020-04-09 14:58:40 -0400

    Switch dataset

--- core/loader/loader.py
+++ core/loader/loader.py
@@ -5,6 +5,6 @@ def load_file():
     """Returns a dataframe containing the data from the csv file (data.csv)
     :returns:pd.DataFrame"""
     try:
-        return pd.read_csv("./core/loader/data.csv", index_col="Rank")
+        return pd.read_csv("./core/loader/anime.csv", index_col="Rank")
     except FileNotFoundError as err:
         print(err)
